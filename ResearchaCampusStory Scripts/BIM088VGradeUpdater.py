788d35de83e673cc497c28ef59d1901aeda639c5
Kevin Chao <Raiden152@yahoo.com>
2023-06-18 04:25:36 -0700

    Updating grading script, removing database clearing script

--- ResearchaCampusStory Scripts/BIM088VGradeUpdater.py
+++ ResearchaCampusStory Scripts/BIM088VGradeUpdater.py
@@ -1,20 +1,27 @@
 # Written by Kevin Chao
-# Last Updated Mar. 11th, 2023
+# Last Updated June 18th, 2023
+#
+# This grading script is meant to be used as a mostly-automated grading tool.
+# Instructions for its use can be found at:
+# https://docs.google.com/document/d/1AgAyvmqwWCodcsrlkgqrolq8TQoxTsO4Y8akFTXkPn0/edit?usp=sharing
 
 from FirebaseInterface import ReSearchDatabase
 from dataclasses import dataclass
 from datetime import datetime
-from dateutil import parser, tz
 from os.path import exists
-import json, csv, pandas
+import csv, pandas
 
 # Easy access constants/variables to prevent user input errors
-
 # Filenames
-# ALL RELEVANT INPUT AND OUTPUT FILES SHOULD BE IN THE SAME DIRECTORY AS THE SCRIPT FOR AN EASIER LIFE
+#   ALL RELEVANT INPUT AND OUTPUT FILES SHOULD BE IN THE SAME DIRECTORY AS THE SCRIPT FOR AN EASIER LIFE
+
+# Filename for the Canvas gradebook the professor provides
 GRADES_INPUT_CSV = '2023 SQ Grades.csv'
+
+# List of filenames for the student rosters the professor provides
 ROSTER_XLS_LIST = ['Section 1 Roster.xls', 'Section 2 Roster.xls']
-DATABASE_JSON = 'researchacampusstory-default-rtdb-export.json'
+
+# Filename for the .csv that will be output that contains student grade information
 GRADES_OUTPUT_CSV = 'BIM 088V SQ2023 UpdatedGrades.csv'
 
 # Column headers in roster
@@ -23,27 +30,14 @@ ROSTER_LAST_NAME = 'Last Name'
 ROSTER_FIRST_NAME = 'First Name'
 ROSTER_EMAIL = 'Email'
 
-# Column headers to be used in output
+# Variable to store column headers from Canvas gradebook to be used in output later
 COLUMN_HEADERS = []
-
-# Firebase database fields
-STUDENTS = 'students'
-SCHEDULE = 'schedule'
-DATES = 'dates'
-DUE_DATES = 'dueDates'
-DATA = 'data'
-LOG = 'log'
-LAST_PROGRESS = 'lastProgress'
-PROGRESS = 'progress'
-USER_INFO = 'userInfo'
-EMAIL = 'email'
-FIRST_NAME = 'firstName'
-LAST_NAME = 'lastName'
-
-DEFAULT = 'default'
 
 # Saved to variable to remain fixed during runtime, script assumed to be run from a machine using Pacific Standard Time
 CUR_DATE_TIME = datetime.now()
+
+# Pull data from database, stored in ReSearchDatabase object
+DATABASE = ReSearchDatabase(CUR_DATE_TIME)
 
 
 
@@ -74,106 +68,30 @@ class PlayerInfo:
 
 
 # Helper Functions
+# Constructs a student roster represented as a dictionary containing the sum of all
+# students from each section that a .xls roster exists for, as stored in ROSTER_XLS_LIST
 def ConstructRoster(rosterList: list[str]) -> dict[int: str]:
     newRoster = dict()
 
     for rosterName in rosterList:
-        rosterDataFrame = pandas.read_excel(rosterName, usecols = [0, 5], header = 8)
+        if exists(rosterName):
+            rosterDataFrame = pandas.read_excel(rosterName, usecols = [0, 5], header = 8)
 
-        for i in range(0, len(rosterDataFrame)):
-            curStudentID = rosterDataFrame[ROSTER_STUDENT_ID][i]
-            curEmail = rosterDataFrame[ROSTER_EMAIL][i]
+            for i in range(0, len(rosterDataFrame)):
+                curStudentID = rosterDataFrame[ROSTER_STUDENT_ID][i]
+                curEmail = rosterDataFrame[ROSTER_EMAIL][i]
 
-            newRoster[curStudentID] = curEmail
+                newRoster[curStudentID] = curEmail
+
+        else:
+            raise Exception(f'Missing expected file: {rosterName}')
 
     return newRoster
 
 
-
-# Deprecate these two
-def ConstructReleaseDateDict(gameFields: dict, numLevels: int) -> dict[int: datetime]:
-    output = dict()
-
-    for levelNum in range(numLevels):
-        parse = parser.parse(gameFields[SCHEDULE][DATES][levelNum])
-
-        output[levelNum] = parse
-
-    return output
-
-def ConstructDueDateDict(gameFields: dict, numLevels: int) -> dict[int: datetime]:
-    output = dict()
-
-    for levelNum in range(numLevels):
-        parse = parser.parse(gameFields[SCHEDULE][DUE_DATES][levelNum])
-
-        output[levelNum] = parse
-
-    
-    return output
-
-
-
-# Assumes all provided times have already been localized to Pacific Standard Time
-def ConstructGradeList(levels: dict, curDateTime: datetime, releaseDateDict: dict, dueDateDict: dict) -> list[int]:
-    progressList = list()
-
-    for levelNum, levelFields in levels.items():
-        # If there is progress data
-        if levelFields[LAST_PROGRESS] != '':
-            # Clean LAST_PROGRESS format to be usable in comparison against due dates
-            lastProgress = parser.parse(levelFields[LAST_PROGRESS])
-
-            # If the level was completed before the due date
-            if lastProgress <= dueDateDict[levelNum] and levelFields[PROGRESS] == 1:
-                progressList.append(1)
-
-            # Otherwise, the level was completed after the due date or was not completed
-            else:
-                progressList.append(0)
-
-        # If there is no progress data
-        else:
-            # If the level has not released yet or the level has been released and the due date has not yet passed
-            if curDateTime < releaseDateDict[levelNum] or curDateTime < dueDateDict[levelNum]:
-                progressList.append(None)
-
-            # Otherwise, if the level has been released and the due date has passed
-            else:
-                progressList.append(0)
-
-    return progressList
-
-# Deprecate
-def ConstructDatabaseGradeDict(gameFields: dict) -> dict[tuple[tuple, str]: list[int]]:
-    newDatabaseGradeDict = dict()
-
-    # Process database data and create updated grade dict
-    for student in gameFields[STUDENTS]:
-        studentFields = gameFields[STUDENTS][student]
-
-        # Grabbing player name for gradeDict keys
-        studentData = studentFields[DATA]
-        curPlayerName = (studentData[USER_INFO][LAST_NAME].strip(' '), studentData[USER_INFO][FIRST_NAME].strip(' '))
-
-        # Grabbing player email for gradeDict keys
-        curPlayerEmail = studentData[USER_INFO][EMAIL].lower()
-
-        # Grabbing grades list for gradeDict values
-        studentProgress = studentFields[PROGRESS]
-        numLevels = len(studentProgress[LOG])
-        releaseDateDict = ConstructReleaseDateDict(gameFields, numLevels)
-        dueDateDict = ConstructDueDateDict(gameFields, numLevels)
-        levels = {levelNum: studentProgress[LOG][levelNum] for levelNum in range(0, numLevels)}
-        curPlayerGrades = ConstructGradeList(levels, CUR_DATE_TIME, releaseDateDict, dueDateDict)
-
-        newDatabaseGradeDict[(curPlayerName, curPlayerEmail)] = curPlayerGrades
-
-    return newDatabaseGradeDict
-
-
-
-def ConstructLoadedGradeDict(csvReader: csv.reader) -> dict[int: PlayerInfo]:
+# Constructs a dictionary from the Canvas gradebook where keys are a student's SIS
+# User ID and values are a PlayerInfo object representing that student
+def LoadStudentDict(csvReader: csv.reader) -> dict[int: PlayerInfo]:
     missingFromRoster = set()
     missingSISUserID = set()
     newLoadedGradeDict = dict()
@@ -217,88 +135,75 @@ def ConstructLoadedGradeDict(csvReader: csv.reader) -> dict[int: PlayerInfo]:
 
     return newLoadedGradeDict
 
-def UpdateLoadedGradeDict(loadedGradeDict: dict, playerRoster: dict, databaseGradeDict: dict) -> dict[int: PlayerInfo]:
-    newLoadedGradeDict = loadedGradeDict.copy()
 
-    # If there were player grades loaded, update the loadedGradeDict with new database data
-    if len(loadedGradeDict) > 0:
-        # Set of players in the loadedGradeDict whose emails don't match any in the database
+# Updates the loadedStudentDict with grade info pulled from the database, returns
+# new loadedStudentDict with updated PlayerInfo
+def UpdateLoadedStudentDict(loadedStudentDict: dict, playerRoster: dict, databaseGradeDict: dict) -> dict[int: PlayerInfo]:
+    newLoadedStudentDict = loadedStudentDict.copy()
+
+    # If there were player grades loaded, update the loadedStudentDict with new database data
+    if len(loadedStudentDict) > 0:
+        # Set of players in the loadedStudentDict whose emails don't match any in the database
         unmatchedPlayers = set()
 
-        for userID, playerInfo in loadedGradeDict.items():
+        for userID, playerInfo in loadedStudentDict.items():
             # Construct playerKey in the format ((LastName, FirstName), Email)
             playerKey = (playerInfo.studentName, playerRoster[playerInfo.userID])
 
-            # Keep track of players from the loadedGradeDict that are not in the database
+            # Keep track of players from the loadedStudentDict that are not in the database
             if playerKey not in databaseGradeDict.keys():
                 unmatchedPlayers.add((', '.join(playerKey[0]), playerKey[1]))
 
-            # If player can be matched to database data, then update grades in loadedGradeDict
+            # If player can be matched to database data, then update grades in loadedStudentDict
             else:
                 for i in range(1, len(playerInfo.grades)):
                     # If grade for level i is not already stored in loaded grades, update with grade from database
                     if playerInfo.grades[i] in [None, '']:
-                        newLoadedGradeDict[userID].grades[i] = databaseGradeDict[playerKey][i - 1]
+                        newLoadedStudentDict[userID].grades[i] = databaseGradeDict[playerKey][i - 1]
 
-        # Display set of players whose loadedGradeDict emails don't match database emails
+        # Display set of players whose loadedStudentDict emails don't match database emails
         if len(unmatchedPlayers) > 0:
             print('===== STUDENTS WITH MISMATCHED EMAILS =====')
             for student in sorted(unmatchedPlayers, key = lambda x: tuple(x[0].split(', '))):
                 print(student)
             print('===========================================\n')
 
-    return newLoadedGradeDict
+    return newLoadedStudentDict
 
 
 
-# Keys are ROSTER Student IDs, values are emails, ideally used to tie database emails to Student ID/SIS User ID
-playerRoster: dict[int: str] = dict()
+if (__name__ == '__main__'):
+    # Keys are ROSTER Student IDs, values are emails, ideally used to tie database emails to Student ID/SIS User ID
+    playerRoster: dict[int: str] = dict()
 
-# Keys are a nested tuple in the format ((LastName, FirstName), Email), values are lists of level grades
-databaseGradeDict: dict[tuple[tuple, str]: list[int]] = dict()
+    # Keys are player's Student IDs/SIS User IDs, values are stored PlayerInfo objects
+    loadedStudentDict: dict[int: PlayerInfo] = dict()
 
-# Keys are player's Student IDs/SIS User IDs, values are stored PlayerInfo objects
-loadedGradeDict: dict[int: PlayerInfo] = dict()
-
-# Load roster from .xls files
-playerRoster = ConstructRoster(ROSTER_XLS_LIST)
+    # Load roster from .xls files
+    playerRoster = ConstructRoster(ROSTER_XLS_LIST)
 
 
-# Convert to using the Firebase Interface
-# Load database instance data
-# database = ReSearchDatabase(CUR_DATE_TIME)
 
-# Load database data from .json file
-if exists(DATABASE_JSON):
-    with open(DATABASE_JSON, 'r') as jsonFile:
-        gameFields = json.load(jsonFile)
-        databaseGradeDict = ConstructDatabaseGradeDict(gameFields)
+    # Load existing Canvas gradebook from .csv file
+    if exists(GRADES_INPUT_CSV):
+        with open(GRADES_INPUT_CSV, 'r') as inputCSVFile:
+            csvReader = csv.reader(inputCSVFile)
+            COLUMN_HEADERS = csvReader.__next__()[0:15] # Grab column headers
+            csvReader.__next__() # Skip next row of column headers, thanks Canvas
+            csvReader.__next__() # Skip Points header (Possible header, wasn't a problem in WQ2023 but was in SQ2023)
+            loadedStudentDict = LoadStudentDict(csvReader)
 
-else:
-    raise Exception(f'Missing expected file: {DATABASE_JSON}')
+        # Update the loadedStudentDict with database data
+        loadedStudentDict = UpdateLoadedStudentDict(loadedStudentDict, playerRoster, DATABASE.gradeDict)
 
+    else:
+        raise Exception(f'Missing expected file: {GRADES_INPUT_CSV}')
 
-# Load existing grades from .csv file
-if exists(GRADES_INPUT_CSV):
-    with open(GRADES_INPUT_CSV, 'r') as inputCSVFile:
-        csvReader = csv.reader(inputCSVFile)
-        COLUMN_HEADERS = csvReader.__next__()[0:15] # Grab column headers
-        csvReader.__next__() # Skip next row of column headers, thanks Canvas
-        csvReader.__next__() # Skip Points Possible header, wasn't a problem in WQ2023 but was in SQ2023
-        loadedGradeDict = ConstructLoadedGradeDict(csvReader)
+    # Write new output .csv
+    with open(GRADES_OUTPUT_CSV, 'w', newline = '') as outputGradesCSV:
+        writer = csv.writer(outputGradesCSV)
+        writer.writerow(COLUMN_HEADERS)
 
-    # Update the loadedGradeDict with database data
-    loadedGradeDict = UpdateLoadedGradeDict(loadedGradeDict, playerRoster, databaseGradeDict)
-
-else:
-    raise Exception(f'Missing expected file: {GRADES_INPUT_CSV}')
-
-
-# Write new .csv
-with open(GRADES_OUTPUT_CSV, 'w', newline = '') as outputGradesCSV:
-    writer = csv.writer(outputGradesCSV)
-    writer.writerow(COLUMN_HEADERS)
-
-    # Sorted by tuples of (LastName, FirstName), this preserves order of the original gradebook
-    for player in sorted(loadedGradeDict.values(), key = lambda x: x.studentName):
-        writer.writerow(player.Unpack())
+        # Sorted by tuples of (LastName, FirstName), this preserves order of the original gradebook
+        for player in sorted(loadedStudentDict.values(), key = lambda x: x.studentName):
+            writer.writerow(player.Unpack())
